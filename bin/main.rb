d2a5751ca57bf0bdaf6df687fc7d8c9fb7987f8f
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-29 15:03:20 -0500

    commit of first method of the scraper app

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/env ruby
-required 'nokogiri'
-required 'httparty'
-required 'rubocop'
+require 'nokogiri'
+require 'httparty'
+require 'rubocop'
+require 'byebug'
 
- 
+def scraper
+  url = 'https://bento.io/tracks'
+  unparsed_page = HTTParty.get(url) 
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  track_listing = parsed_page.css('div.track-content')
+  byebug
+end
+
+scraper
+
