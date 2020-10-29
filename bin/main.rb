712db90098337c49f52c23deb45623534792997b
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-29 16:51:40 -0500

    added varibles and arrays to save the data from tracks

--- bin/main.rb
+++ bin/main.rb
@@ -5,18 +5,26 @@ require 'rubocop'
 require 'byebug'
 
 def scraper
+  attr_reader :track_arr
+
   url = 'https://bento.io/tracks'
   unparsed_page = HTTParty.get(url) 
   parsed_page = Nokogiri::HTML(unparsed_page)
+  track_arr = []
   track_listing = parsed_page.css('a.track') # 20 tracks
   track_listing.each do |track_listing|
     tracks = {
       title: track_listing.css('div.track-content')[0].children.children[0].text,
-      description:  track_listing.css('div.track-content')[0].children.children[1].text,
-      url: 'https://bento.io' + track_listing["href"],
-    } 
+      description: track_listing.css('div.track-content')[0].children.children[1].text,
+      url: "https://bento.io#{track_listing['href']}"
+    }
+    track_arr << tracks
   end
-  byebug
 end
 
-scraper
+def first_message
+  'this is the scraper app for Bento.io web page'
+end
+
+
+
