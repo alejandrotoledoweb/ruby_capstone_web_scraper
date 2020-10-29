87782ad5c6ab56bb4fba0dbe1733094a0037facd
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-29 17:03:10 -0500

    fixed syntax errors

--- bin/main.rb
+++ bin/main.rb
@@ -9,19 +9,20 @@ class Scraper
     attr_reader :track_arr, :number_user
   
     @number_user = gets.chomp
-  if @number_input.include?(1..20)
-    url = 'https://bento.io/tracks'
-    unparsed_page = HTTParty.get(url) 
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    track_arr = []
-    track_listing = parsed_page.css('a.track') # 20 tracks
-    track_listing.each do |track_listing|
-      tracks = {
-        title: track_listing.css('div.track-content')[0].children.children[0].text,
-        description: track_listing.css('div.track-content')[0].children.children[1].text,
-        url: "https://bento.io#{track_listing['href']}"
-      }
-      track_arr << tracks
+    if @number_input.include?(1..20)
+      url = 'https://bento.io/tracks'
+      unparsed_page = HTTParty.get(url) 
+      parsed_page = Nokogiri::HTML(unparsed_page)
+      track_arr = []
+      track_listing = parsed_page.css('a.track') # 20 tracks
+      track_listing.each do |track_listing|
+        tracks = {
+          title: track_listing.css('div.track-content')[0].children.children[0].text,
+          description: track_listing.css('div.track-content')[0].children.children[1].text,
+          url: "https://bento.io#{track_listing['href']}"
+        }
+        track_arr << tracks
+      end
     else
       'please enter a valid number'
     end
