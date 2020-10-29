e10725c31debbf11e1a0a6259bae5182e3861a61
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-29 17:57:15 -0500

    fixed syntax issues with block

--- bin/main.rb
+++ bin/main.rb
@@ -9,15 +9,15 @@ class ScraperClass
 
   def scraper
     url = 'https://bento.io/tracks'
-    unparsed_page = HTTParty.get(url) 
+    unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
     track_arr = []
     track_listing = parsed_page.css('a.track') # 20 tracks
-    track_listing.each do |track_listing|
+    track_listing.each do |track_item|
       tracks = {
-        title: track_listing.css('div.track-content')[0].children.children[0].text,
-        description: track_listing.css('div.track-content')[0].children.children[1].text,
-        url: "https://bento.io#{track_listing['href']}"
+        title: track_item.css('div.track-content')[0].children.children[0].text,
+        description: track_item.css('div.track-content')[0].children.children[1].text,
+        url: "https://bento.io#{track_item['href']}"
       }
       track_arr << tracks
     end
