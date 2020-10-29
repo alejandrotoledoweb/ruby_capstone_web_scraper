7def380079eee8cb482044ece2a57239183de9e4
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-29 16:14:47 -0500

    added the url, h3 and p from each track on the page

--- bin/main.rb
+++ bin/main.rb
@@ -8,9 +8,15 @@ def scraper
   url = 'https://bento.io/tracks'
   unparsed_page = HTTParty.get(url) 
   parsed_page = Nokogiri::HTML(unparsed_page)
-  track_listing = parsed_page.css('div.track-content')
+  track_listing = parsed_page.css('a.track') # 20 tracks
+  track_listing.each do |track_listing|
+    tracks = {
+      title: track_listing.css('div.track-content')[0].children.children[0].text,
+      description:  track_listing.css('div.track-content')[0].children.children[1].text,
+      url: 'https://bento.io' + track_listing["href"],
+    } 
+  end
   byebug
 end
 
 scraper
-
