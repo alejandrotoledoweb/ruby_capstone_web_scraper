fa27a4abd4ab88896130a07c7385eeca3c4cc20c
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-02 10:57:27 -0500

    add new spec for string for track information method scrap_web

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -5,8 +5,7 @@ require './lib/scraper'
 describe ScraperClass do
   let(:test_var) { ScraperClass.new('https://bento.io/tracks') }
   let(:test_output) { 'test_var.print_data' }
-  let(:parsed_var) { test_var.scraper }
-  let(:all_tracks) { parsed_var.scraper_tracks}
+  let(:parsed_var) { ScraperClass.new('https://bento.io/topic/web') }
   describe '#scraper' do
     it 'test if the output is an object' do
       expect(test_var.track_arr.class.superclass).to eql(Object)
@@ -22,6 +21,13 @@ describe ScraperClass do
   describe '#scraper' do
     it 'returns the Array with the info' do
       expect(test_var.class.superclass).to eql(Object)
+    end
+  end
+
+  describe '#scrap_web' do
+    result = ''
+    it 'test the result to be a String' do
+      expect(result.class).to eql(String)
     end
   end
 end
